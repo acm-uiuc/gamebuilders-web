979102f748adcb4814bfdd978a02cd00747da133
Mega-Baboon <61724429+Mega-Baboon@users.noreply.github.com>
2020-06-12 16:59:03 -0500

    Updated Header
    Sound is now off by default

--- titlegame/uph_set_canvas_size.js
+++
@@ -1,7 +0,0 @@
-function set_canvas_size() {
-	var canvas = document.getElementById('canvas');
-	var pixelRatio = window.devicePixelRatio; 
-	canvas.style.width = (canvas.width / pixelRatio) + "px";
-	canvas.style.height = (canvas.height / pixelRatio) + "px";
-	console.log('eee');
-}
